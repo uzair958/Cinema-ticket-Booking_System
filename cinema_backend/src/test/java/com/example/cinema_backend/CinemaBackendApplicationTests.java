61373830487e4e7a2b502370dbeca07bb38fbbfc
uzair958 <uzair958@gmail.com>
2025-10-19 20:57:23 +0500

    initial setup

---
+++ cinema_backend/src/test/java/com/example/cinema_backend/CinemaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinema_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
